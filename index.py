8b17e6dcf6f0de03f94ba337aa8f78997838191f
Sreenivassreeee <ksreenivascse@gmail.com>
2023-11-02 17:20:15 +0530

    draft final

--- index.py
+++ index.py
@@ -19,27 +19,62 @@ def getMainCatorogiesLinks():
 
 
 main_catorogies_links=getMainCatorogiesLinks()
-print("TOTal Main Catorory Links : "+str(len(main_catorogies_links)))
-company_links=[]
+print("Total Main Catorory Links : "+str(len(main_catorogies_links)))
 for catorogy_link in main_catorogies_links:
+    company_links=[]
     for page in range(1,100):
         print("Currectly Working on : "+str(page))
-        catorogy_response = requests.get(base_url+catorogy_link+"/page-"+str(page))
+        final_catogary_link=base_url+catorogy_link+"/page-"+str(page)
+        catorogy_response = requests.get(final_catogary_link)
         if catorogy_response.status_code == 200:
             soup = BeautifulSoup(catorogy_response.text, 'html.parser')
             list_of_companies=soup.select_one('#List-of-companies')
-            print(page)
-            print(list_of_companies)
-            for company in list_of_companies:
-                company_link=company.find('dt').find('a').get('href')
-                company_links.append(company_link)
+            if list_of_companies:
+                for company in list_of_companies:
+                    if company.find('dt').find('a').get('href'):
+                        company_link=company.find('dt').find('a').get('href')
+                        write_to_excel(final_catogary_link,company_link)
+                    else:
+                        break
+            else:
+                break
+            
         else:
             break
-    break
+    
+def write_to_excel(catorogy_link):
+    each_company_response = requests.get(company_url)
+    soup = BeautifulSoup(each_company_response.text, 'html.parser')
+    company_div=soup.select_one('#main')
+    company_name=company_div.soup.find('h1')
+    data_table=soup.xpath('/html/body/div/div[2]/div[3]/div[1]/div/dl[2]/dd/table/tbody')
+    business_type=''
+    address=''
+    for tr in data_table:
+        col_name=tr.find('td')
+        if col_name=='Businss Type:':
+            business_type=tr.find('td')
+        elif col_name=='Address:'
+            address==tr.find('td')
+    catorogy_link=final_catogary_link
+    company_link=company_link
+    print(catorogy_link)
+    print(company_link)
+    print(company_name)
+    print(business_type)
+    print(address)
+
+    df = pd.DataFrame({'catorogy_link': catorogy_link,"company_link":company_link,"company_name":company_name,"business_type":business_type,"address":address})
+    excel_file='Data.csv'
+    df.to_csv(excel_file, index=False)
+    print(f"Data saved to {excel_file}")
+print(catorogy_link)
+print("------------------Hitting next -----------")
 # print(len(company_links))
 
 
-
+# /html/body/div/div[2]/div[3]/div[1]/div/dl[2]/dd/table/tbody
+# /html/body/div/div[2]/h1
 
 
 
@@ -68,4 +103,3 @@ for catorogy_link in main_catorogies_links:
 #     excel_file = 'C:\\Users\\kesimi.r.reddy\\OneDrive - Accenture\\U4b\\ItalyClawler.xlsx'
 #     df.to_excel(excel_file, index=False)
 #     print(f"Data saved to {excel_file}")
-
