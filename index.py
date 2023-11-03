63cc0940012fdebf7db6ece3d6fc98c04b8c2dcc
Sreenivassreeee <ksreenivascse@gmail.com>
2023-11-03 20:13:56 +0530

    fixed the looping issue

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
-
+import time
 base_url="http://www.business-yellowpages.com"
 
 excel_file='Data.csv'
@@ -18,15 +18,17 @@ def write_to_csv(catorogy_link,company_url,company_name,business_type,address):
     company_names.append(company_name)
     business_types.append(business_type)
     addresses.append(address)
-    print(f"Added {company_name}")
+    print(f"Added {catorogy_link}--   {company_name}--   {business_type}")
 
 
 
 def read_company_details(catorogy_link,company_url):
+    
     each_company_response = requests.get(company_url)
+    time.sleep(1)
     soup = BeautifulSoup(each_company_response.text, 'html.parser')
     company_div=soup.select_one('#main')
-    company_name=company_div.find('h1').text
+    company_name=company_div.find('h1').text if company_div.find('h1') else "None"
     data_table=soup.select_one('#company-detail')
     data_table=data_table.findAll('div')[1].findAll('dl')[1].find('dd').find('table')
     business_type=''
@@ -44,34 +46,39 @@ def read_company_details(catorogy_link,company_url):
 
 def getMainCatorogiesLinks():
     response = requests.get(base_url+"/italy/")
+    time.sleep(1)
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    lis=[]
     links =soup.select_one('#list_top_product_country > ul > ul')
     for li in links.findAll('li'):
         if li.find('a'):
             catorogy_link=li.find('a').get('href')
             company_links=[]
             for page in range(1,100):
-                print("Currectly Working on : "+str(page))
                 final_catogary_link=base_url+catorogy_link+"/page-"+str(page)
+                print("Currectly Working on Page : "+str(catorogy_link+"/page-"+str(page)))
+
                 catorogy_response = requests.get(final_catogary_link)
-                if catorogy_response.status_code == 200:
-                    soup = BeautifulSoup(catorogy_response.text, 'html.parser')
-                    list_of_companies=soup.select_one('#List-of-companies')
-                    if list_of_companies[0]!=None:
-                        for company in list_of_companies:
+                time.sleep(1)
+                soup = BeautifulSoup(catorogy_response.text, 'html.parser')
+                list_of_companies=soup.select_one('#List-of-companies')
+                # print(list_of_companies)
+                try:
+                    for company in list_of_companies:
+                        if company.find('dt')!=None:
                             if company.find('dt').find('a').get('href'):
                                 company_link=company.find('dt').find('a').get('href')
                                 read_company_details(final_catogary_link,company_link)
-                                break
                             else:
                                 break
+                        else:
+                            break
                     else:
                         break
+                except:
+                    print("Error")
+            continue
 
-                else:
-                    break
-    return lis
 
 
 getMainCatorogiesLinks()
